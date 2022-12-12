1e2a7b7123b2a5a77db6c5fe60e238454529df67
Felipe Gamer <100796428+fetuber4095@users.noreply.github.com>
2022-12-12 12:36:49 -0300

    snaphot a12w12 - IAS: Intelligent Authenticate Setup
    Now you can login with an universal acount Its mean that you can acess the UNIVERSAL CONSIENCE SERVER

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from os import system
 from time import sleep as delay
 from random import randint 
+from getpass import getpass
 
 __version__ = ["alpha 1.0", "Kernel Implemetation"]
 
@@ -21,6 +22,24 @@ if __name__ == '__main__':
 		login.close()
 	except FileNotFoundError:
 		login = open("LOGIN", "wt+")
+		print("Hello, Welcome to the UNIVERSAL CONSIENCE CONSOLE")
+		print("To continue you need create an username to log at Server\n\n")
+		username = input("login: ").split()
+		password = getpass("password: ").split()
+		print("\n\nNow you are registred!")
+		print("The Universal Console is a Protected Software, you need be authorized to acess")
+		print("If you have a special Key please insert here. Else press ENTER!")
+		entered_key = input("KEY: ")
+		if entered_key == __version__[2]:
+			entered_key = True
+			print(f"{username}, your account have permission to acess the SECRET SERVER"), sleep(5)
+		else:
+			entered_key = False
+			clear()
+			print(f"Oh {username}, You arent a PERMITTED USER!!!"), sleep(5)
+
+		login.write(f"{username} {password} {entered_key}")
+		login.close()
 		Kernel()
 	else:
 		Kernel()
